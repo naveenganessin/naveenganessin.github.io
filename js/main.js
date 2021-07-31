fb4be2167d946d2bdd4c459276a4185ffda8edb4
naveenganessin <naveenganessin@gmail.com>
2021-07-31 15:38:34 -0400

    Added initial files and title

---
+++ js/main.js
@@ -0,0 +1,8 @@
+async function initialize() {
+    d3.select('svg').append('text')
+        .attr('x', 10)
+        .attr('y', 50)
+        .attr('stroke', 'Black')
+        .style("font-size", 40)
+        .text("Was Michael leaving the end of it?")
+}
